b634d51297959bee38f97d5962c166938d5803b2
danu <danu.sulistiyan@pizzahut.co.id>
2026-05-08 16:46:25 +0700

    fix initial setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Project level build.gradle.kts
 
 plugins {
-    id("com.android.application") version "8.1.0" apply false
+    id("com.android.application") version "8.2.2" apply false
     kotlin("android") version "1.9.10" apply false
     kotlin("jvm") version "1.9.10" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
